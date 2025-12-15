906d57c7ffd3df0ed65b41a9b49d64958874b984
coderjc <coderjc@qq.com>
2025-12-15 14:21:49 +0800

    chore: 添加生成文档目录的脚本和package.json配置

---
+++ scripts/gen-toc.js
@@ -0,0 +1,151 @@
+// 读取文件目录
+const fs = require('fs')
+const path = require('path')
+const { globSync } = require('glob')
+const matter = require('gray-matter')
+
+const DocsDir = path.join(process.cwd(), 'docs')
+
+/**
+ * 生成节点
+ * @param {*} names
+ */
+function genNode(key, parentKey) {
+    const isFile = key.endsWith('.md')
+
+    const filename = key.split(path.sep).pop()
+
+    const node = {
+        filename: isFile ? filename.replace(/\.md$/, '') : filename,
+        path: `.${path.sep}docs${path.sep}${key}`, // 相对地址
+        children: isFile ? null : [],
+        key,
+        parentKey
+    }
+
+    return node
+}
+
+/**
+ * 生成文件树
+ */
+function genTree(nodes, parentKey = null) {
+    const tree = []
+    nodes
+        .filter(item => item.parentKey === parentKey)
+        .forEach(item => {
+            const children = genTree(nodes, item.key)
+            if (children.length) {
+                item.children = children
+            }
+            tree.push(item)
+        })
+    return tree
+}
+
+/**
+ * 构建节点列表
+ */
+function buildNodes(files) {
+    const nodes = []
+    files.forEach(item => {
+        buildNode(item, nodes)
+    })
+
+    function buildNode(paths, nodes) {
+        let preNode = null
+        while (paths.length) {
+            const name = paths.shift()
+            const node = genNode(name, preNode ? preNode.key : null)
+            nodes.push(node)
+            preNode = node
+        }
+    }
+
+    return nodes
+}
+
+/**
+ * 根据 key 去重
+ */
+function uniqByKey(arr) {
+    return Array.from(new Map(arr.map(item => [item.key, item])).values())
+}
+
+/**
+ * 获取所有 markdown 文件列表
+ */
+function getAllMdFiles(dir) {
+    const files = globSync(`**${path.sep}*.md`, { cwd: dir }).map(item => {
+        const paths = item.split(path.sep)
+        return paths.map((_, i) => paths.slice(0, i + 1).join(path.sep))
+    })
+
+    return files
+}
+
+/**
+ * 递归遍历树
+ */
+function traverseTree(nodes, handler) {
+    if (!Array.isArray(nodes)) return
+
+    nodes.forEach(node => {
+        handler(node)
+
+        if (Array.isArray(node.children)) {
+            traverseTree(node.children, handler)
+        }
+    })
+}
+
+/**
+ * 根据树结构生成目录
+ */
+function generateToc(tree, level = 0) {
+    const indent = '  '.repeat(level)
+    let result = ''
+
+    for (const node of tree) {
+        // 目录
+        if (Array.isArray(node.children)) {
+            result += `${indent}- ${node.filename}\n`
+            result += generateToc(node.children, level + 1)
+        }
+        // Markdown 文件
+        else {
+            result += `${indent}- [${node.filename}](${node.path})\n`
+        }
+    }
+
+    return result
+}
+
+/**
+ * 将 toc 写入文件
+ */
+function writeTocToFile(toc, filePath) {
+    fs.writeFileSync(filePath, toc, 'utf-8')
+}
+
+/**
+ * 主函数
+ */
+function main() {
+    const files = getAllMdFiles(DocsDir)
+    const nodes = uniqByKey(buildNodes(files))
+    const nodeTrees = genTree(nodes)
+    traverseTree(nodeTrees, node => {
+        if (node.path.endsWith('.md')) {
+            const filePath = path.join(process.cwd(), node.path)
+            const fileContent = fs.readFileSync(filePath, 'utf-8')
+            const { data } = matter(fileContent)
+            node.meta = data
+        }
+    })
+    const toc = generateToc(nodeTrees)
+    const tocFilePath = path.join(process.cwd(), 'README.md')
+    writeTocToFile(toc, tocFilePath)
+    console.log('TOC generated successfully!')
+}
+main()
